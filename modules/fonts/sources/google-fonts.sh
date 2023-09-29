196183e883dac9aa2ae826d9337af6e90fc9fb9b
lucasvsr <lucasvsr@users.noreply.github.com>
2023-09-29 17:47:38 -0300

    chore: fonts module

--- modules/fonts/sources/google-fonts.sh
+++ modules/fonts/sources/google-fonts.sh
@@ -9,6 +9,8 @@ if [ ${#FONTS[@]} -gt 0 ]; then
 
     echo "Installation of google-fonts started"
 
+    echo "${COMPRESS_TYPES[@]}" | while IFS=$'\n' read -r type; do
+
     for font in "${FONTS[@]}"; do
 
         font="$(echo "$font" | tr -d '\n')"
