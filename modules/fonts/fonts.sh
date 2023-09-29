b9e49314487e3524c1f8307c493e070daa0bc27d
lucasvsr <lucasvsr@users.noreply.github.com>
2023-09-29 15:51:33 -0300

    chore: fonts module

--- modules/fonts/fonts.sh
+++ modules/fonts/fonts.sh
@@ -3,16 +3,19 @@ set -oue pipefail
 
 get_yaml_array NERD '.fonts.nerd-fonts[]' "$1"
 get_yaml_array GOOGLE '.fonts.google-fonts[]' "$1"
+
 export FONTS_MODULE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+
 
 if [ ${#GOOGLE[@]} -gt 0 ]; then
 
-    "$FONTS_MODULE_DIR"/sources/google-fonts.sh "${GOOGLE[@]}"
+    eval "$FONTS_MODULE_DIR"/sources/google-fonts.sh "${GOOGLE[@]}"
 
 fi
 
 if [ ${#NERD[@]} -gt 0 ]; then
 
-    "$FONTS_MODULE_DIR"/sources/nerd-fonts.sh "${NERD[@]}"
+    eval "$FONTS_MODULE_DIR"/sources/nerd-fonts.sh "${NERD[@]}"
 
 fi
