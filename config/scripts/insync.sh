3b72fcd566c4fd52c7f90f7b3718abafbeae96f1
Lucas Ribeiro <lucasvsribeiro1@gmail.com>
2023-11-17 08:03:21 -0300

    Update insync.sh

--- config/scripts/insync.sh
+++ config/scripts/insync.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-sudo rpm --import https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
+REPO="https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key"
+
+if sudo rpm --import $REPO
+then
+
+  echo "Chave de importação do Insync importada com sucesso."
+
+fi
 
 mkdir -p /etc/yum.repos.d/
 
 echo "[insync]
 name=insync repo
-baseurl=http://yum.insync.io/fedora/$releasever/
+baseurl=http://yum.insync.io/fedora/$IMAGE_MAJOR_VERSION/
 gpgcheck=1
-gpgkey=https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
+gpgkey=$REPO
 enabled=1
 metadata_expire=120m" > /etc/yum.repos.d/insync.repo
