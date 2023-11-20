6da00e88cbdb2c5057529c99f18b9260a99c0531
lucasvsr <lucasvsr@users.noreply.github.com>
2023-11-20 11:29:25 -0300

    feat: gnome-extensions

--- modules/gnome-extensions/install.sh
+++ modules/gnome-extensions/install.sh
@@ -6,25 +6,23 @@ VERSION=$2
 
 download() {
 
-    local zip="$1.$2.shell-extension.zip"
+    local zip="$1.v$2.shell-extension.zip"
     local extensions_dir="/usr/share/gnome-shell/extensions"
     local download_dir="/tmp/${zip//@/}"
 
-    if [ -d "${extensions_dir}/$1" ]; 
-    then
+    if [ -d "${extensions_dir}/$1" ]; then
 
         echo "$1 já instalada"
 
     else
 
-        if curl https://extensions.gnome.org/extension-data/"${zip//@/}" > "${download_dir}"
-        then
+        if curl https://extensions.gnome.org/extension-data/"${zip//@/}" >"${download_dir}"; then
 
             echo "$1 baixado com sucesso."
             echo "Iniciando instalação"
             mkdir -p "$extensions_dir/$1"
             unzip "${download_dir}" -d "$extensions_dir/$1"
-            
+
         fi
 
     fi
