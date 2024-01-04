12a30e64190e21c936582f28a5ac937a7030d47a
lucasvsr <lucasvsr@users.noreply.github.com>
2024-01-04 11:24:48 -0300

    feat: espanso

--- config/scripts/espanso.sh
+++ config/scripts/espanso.sh
@@ -9,8 +9,8 @@ echo "Instalando as dependências do Espanso via rpm-ostree"
 rpm-ostree install cargo make automake gcc gcc-c++ kernel-devel wl-clipboard libxkbcommon-devel dbus-devel wxGTK-devel.x86_64
 
 echo "Instalando as dependências do Espanso via cargo"
-# sudo useradd --system --create-home ${CARGO_USER} && echo "$CARGO_USER ALL=(ALL:ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/${CARGO_USER}
-# su - ${CARGO_USER}
+sudo useradd --system --create-home ${CARGO_USER} && echo "$CARGO_USER ALL=(ALL:ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/${CARGO_USER}
+sudo su - ${CARGO_USER}
 sudo cargo install --force cargo-make --version 0.34.0
 
 git clone https://github.com/federico-terzi/espanso /tmp/espanso
@@ -21,4 +21,4 @@ sudo cargo make --profile release --env NO_X11=true build-binary
 mv target/release/espanso /usr/local/bin/espanso
 
 setcap "cap_dac_override+p" "$(which espanso)"
-# sudo su
+sudo su
