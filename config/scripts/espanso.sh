18711c4c2a5c8ec196870b926f34514690727490
lucasvsr <lucasvsr@users.noreply.github.com>
2024-01-04 09:56:25 -0300

    feat: espanso

--- config/scripts/espanso.sh
+++ config/scripts/espanso.sh
@@ -13,6 +13,7 @@ cargo install --force cargo-make --version 0.34.0
 git clone https://github.com/federico-terzi/espanso /tmp/espanso
 
 cd /tmp/espanso
+rm -rf /root/.cargo
 cargo make --profile release --env NO_X11=true build-binary 
 
 mv target/release/espanso /usr/local/bin/espanso
