b4bf8b1a3b1ae65d19c8d8c9821704408e3ad318
lucasvsr <lucasvsr@users.noreply.github.com>
2024-01-04 10:12:02 -0300

    feat: espanso

--- config/scripts/espanso.sh
+++ config/scripts/espanso.sh
@@ -8,12 +8,12 @@ echo "Instalando as dependências do Espanso via rpm-ostree"
 rpm-ostree install cargo make automake gcc gcc-c++ kernel-devel wl-clipboard libxkbcommon-devel dbus-devel wxGTK-devel.x86_64
 
 echo "Instalando as dependências do Espanso via cargo"
+rm -rf /root/.cargo
 cargo install --force cargo-make --version 0.34.0
 
 git clone https://github.com/federico-terzi/espanso /tmp/espanso
 
 cd /tmp/espanso
-rm -rf /root/.cargo
 cargo make --profile release --env NO_X11=true build-binary 
 
 mv target/release/espanso /usr/local/bin/espanso
